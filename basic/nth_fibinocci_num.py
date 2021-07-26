611f705947066e5ad111e7243d257ce4035032b5
Geetha Medisetty <geetha.medisetty@gmail.com>
2021-07-26 00:44:01 -0700

    comitting code

--- basic/nth_fibinocci_num.py
+++ basic/nth_fibinocci_num.py
@@ -1,7 +1,6 @@
 """
 Python Program for n-th Fibonacci number
-Difficulty Level : Easy
-Last Updated : 11 Jun, 2021
+
 In mathematical terms, the sequence Fn of Fibonacci numbers is defined by the recurrence relation
 
 Fn = Fn-1 + Fn-2
@@ -9,3 +8,24 @@ With seed values
 
 F0 = 0 and F1 = 1.
 """
+import ast
+
+
+def fibonacci_generator(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci_generator(n - 1) + fibonacci_generator(n - 2)
+
+
+try:
+    nth_num = ast.literal_eval(input("Enter n"))
+    if nth_num <= 0:
+        print("Enter a valid input")
+    else:
+        nth_fibonacci_num = fibonacci_generator(nth_num)
+    print(nth_fibonacci_num)
+except Exception as error:
+    print("Please enter a Integer", error)
