aec730c71abd3a5129faacd7133443c60f465a7d
Geetha Medisetty <geetha.medisetty@gmail.com>
2021-07-25 19:32:18 -0700

    prime or not

---
+++ basic/prime_or_not.py
@@ -0,0 +1,8 @@
+"""
+Python program to check whether a number is Prime or not
+Difficulty Level : Easy
+Last Updated : 12 Jul, 2021
+Given a positive integer N, The task is to write a Python program to check if the number is prime or not.
+Definition: A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+The first few prime numbers are {2, 3, 5, 7, 11, ….}.
+"""
