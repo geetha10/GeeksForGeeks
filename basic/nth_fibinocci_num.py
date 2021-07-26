fbbe181a5e13fa0895c90dba8d32ce3c2092dfdd
Geetha Medisetty <geetha.medisetty@gmail.com>
2021-07-25 19:36:25 -0700

    nth fibonacci num

---
+++ basic/nth_fibinocci_num.py
@@ -0,0 +1,11 @@
+"""
+Python Program for n-th Fibonacci number
+Difficulty Level : Easy
+Last Updated : 11 Jun, 2021
+In mathematical terms, the sequence Fn of Fibonacci numbers is defined by the recurrence relation
+
+Fn = Fn-1 + Fn-2
+With seed values
+
+F0 = 0 and F1 = 1.
+"""
